3a10acc78900bc57e8b4e8f5eed0f1d77c7e0e4a
NTH413-PC\NTH413 <test@test.com>
2017-07-31 16:53:24 +0900

    1

--- board/routes/users.js
+++ board/routes/users.js
@@ -1,7 +1,6 @@
 var express = require("express");
 var router = express.Router();
 var User = require("../models/User");
-var util = require("../util");
 
 //Index //1
 router.route("/").get(function(req, res){
@@ -22,21 +21,22 @@ router.get("/new", function(req, res){
 
 //create
 router.post("/", function(req, res){
- User.create(req.body, function(err, user){
-  if(err){
-   req.flash("user", req.body);
-   req.flash("errors", util.parseError(err)); // 1
-   return res.redirect("/users/new");
-  }
-  res.redirect("/users");
- });
+  User.create(req.body, function(err, user){
+    if(err){
+      req.flash("users", req.body);
+      req.flash("errors", parseError(err));
+      return res.redirect("/users/new");
+    }
+    res.render("users/confirm");
+  });
 });
 
 //show
 router.get("/:username", function(req, res){
   User.findOne({username:req.params.username}, function(err, user){
     if(err) return res.json(err);
-    res.render("users/show", {user:user});
+    res.render("users/show_others", {user:user});
+
   });
 });
 
@@ -61,11 +61,11 @@ router.get("/:username/checkmsg", function(req, res){
 
 
 //update //2
-router.put("/:username",function(req, res, next){
- User.findOne({username:req.params.username})
- .select({password:1})
- .exec(function(err, user){
-  if(err) return res.json(err);
+router.put("/:username", function(req, res, next){
+  User.findOne({username:req.params.username})  //2-1
+  .select("password:1") //2-2. select 함수를 이용하면 DB에서 어떤 항목을 선택할지 안할지 정할 수 있다.
+  .exec(function(err, user){
+    if(err) return res.json(err);
 
     //update user object
     user.originalPassword = user.password;
@@ -78,11 +78,10 @@ router.put("/:username",function(req, res, next){
     user.save(function(err, user){
      if(err){
       req.flash("user", req.body);
-      req.flash("errors", util.parseError(err)); // 1
+      req.flash("errors", parseError(err));
       return res.redirect("/users/"+req.params.username+"/edit");
      }
-     res.redirect("/users/"+req.params.username);
-    });
+     res.redirect("/users/"+user.username);  });
    });
   });
 
