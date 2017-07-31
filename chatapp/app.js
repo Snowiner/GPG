e7e7e46643ce606680c4c0155b2e85d4ee409b33
Terryu\Terry <anhj0131@naver.com>
2017-07-31 19:19:01 +0900

    port changed to 3003

--- chatapp/app.js
+++ chatapp/app.js
@@ -4,7 +4,7 @@ var  server = require('http').createServer(app);
 var  io = require('socket.io').listen(server);
 var mongoose = require('mongoose');
 var users = {};
-server.listen(3000);
+server.listen(3003);
 
 mongoose.connect(process.env.MONGO_DB, function(err){
   if(err){
