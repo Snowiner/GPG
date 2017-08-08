ff52c0197630db1c1cc88ed69f165e07b7d7f427
NTH413-PC\NTH413 <test@test.com>
2017-08-08 14:01:29 +0900

    브랜치의 시작

--- board/routes/news_.js
+++ board/routes/news_.js
@@ -6,7 +6,7 @@ var request = require('request');
 var cheerio = require('cheerio');
 var iconv = require('iconv-lite');
 var options = {
-  url:'http://media.daum.net/breakingnews/digital',
+  url:'http://media.daum.net/breakingnews/digital'
 
 };
 
@@ -15,10 +15,7 @@ router.get('/',function(req,res){
   request.get(options , function(err,res,html){
     var $ = cheerio.load(html);
 
-  var data = $(".list_news2 list_allnews[0]").text();
-
-
-      console.log(data);
+      console.log($);
 
   });
 
