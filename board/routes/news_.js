8faee7c286e2abcde1e1ff6f76241c48f07affad
NTH413-PC\NTH413 <test@test.com>
2017-08-09 17:56:57 +0900

    no message

--- board/routes/news_.js
+++ board/routes/news_.js
@@ -2,22 +2,65 @@ var express = require('express');
 var router = express.Router();
 var User = require("../models/User");
 
-var request = require('request');
 var cheerio = require('cheerio');
-var iconv = require('iconv-lite');
-var options = {
-  url:'http://media.daum.net/breakingnews/digital'
+var request = require('request');
 
-};
+var url = 'http://media.daum.net/breakingnews/digital';
 
-router.get('/',function(req,res){
 
-  request.get(options , function(err,res,html){
-    var $ = cheerio.load(html);
 
-      console.log($);
 
-  });
+router.get('/',function(req,ress){
+
+
+
+
+
+request( url , function( err , response, html){
+
+
+  var link = [];
+  var title = [];
+  var des = [];
+  var img = [];
+
+
+  if( err ){ throw err };
+
+  var $ = cheerio.load(html);
+  var arr = $('.cont_thumb .tit_thumb > a').length;
+  var arr_ = $('.desc_thumb').length;
+
+
+
+  $('.cont_thumb .tit_thumb > a').each(function( i , elem){
+    link[i] = $(this).attr("href");
+    title[i] = $(this).text();
+  })
+$('.cont_thumb .desc_thumb').each(function( i , elem){
+  des[i] = $(this).text();
+})
+
+
+  //
+  // $('.tit_thumb').forEach(function(data){
+  //   console.log(  data('.link_txt').attr("href"));
+  // });
+  // var tmp = [];
+  //s
+  // $('.cont_thumb').each(function( i , elem){
+  //   tmp[i].title = $(this).(' .tit_thumb').text();
+  //   tmp[i].link = $(this).(' .tit_thumb > a').attr("href");
+  //   tmp[i].des = $(this).(' .desc_thumb').text();
+  // })
+
+   response.render("news/news_",{link:link,title:title,des:des});
+
+
+
+
+})
+// ress.render("news/news_",{link:link,title:title,des:des});
 
 });
 
